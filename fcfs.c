9ae96d6dc257c1ec5935ac658996d62279e66332
antiquark007 <141497727+antiquark007@users.noreply.github.com>
2025-03-25 18:35:41 +0530

    updated

--- fcfs.c
+++ fcfs.c
@@ -1,48 +1,46 @@
-#include<stdio.h>
-int main(){
-    int wt[20],bt[20],tat[20],at[20],ct[20],i,n,temp=0;
-    float wtavg,tatavg;
+#include <stdio.h>
+
+int main() {
+    int n, i, at[20], bt[20], wt[20] = {0}, ct[20], tat[20];
+    float wtavg = 0, tatavg = 0;
+
     printf("Enter the number of processes: ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        printf("\nEnter arrival and burst time for process %d: ", i);
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("Enter arrival and burst time for process %d: ", i);
         scanf("%d %d", &at[i], &bt[i]);
     }
-    temp=bt[0];
-    ct[0]=bt[0];
-    wt[0]=0;
-    tat[0]=bt[0]-at[0];
-    wtavg=0;
-    tatavg=bt[0];
-    for(i=1;i<n;i++){
-        temp+=bt[i];
-        ct[i]=temp;
-        wt[i]=ct[i-1]-at[i];
-        tat[i]=ct[i]-at[i];
-        wtavg+=wt[i];
-        tatavg+=tat[i];
+
+    ct[0] = bt[0];
+    tat[0] = ct[0] - at[0];
+    wt[0] = 0;
+
+
+    for (i = 1; i < n; i++) {
+        ct[i] = ct[i - 1] + bt[i];  // Completion time
+        wt[i] = ct[i - 1] - at[i];  // Waiting time
+        tat[i] = ct[i] - at[i];     // Turnaround time
+        wtavg += wt[i];
+        tatavg += tat[i];
     }
+
     wtavg /= n;
-    tatavg /= n;
-    
+    tatavg = (tatavg + tat[0]) / n; 
+
     printf("\n\tPROCESS\tARRIVAL_T\tBURST_T\tCOMPLETE_T\tWAITING_T\tTURN_AROUND_T\n");
-    printf("\t-------\t---------\t-------\t----------\t---------\t--------------\n");
-    for(i=0;i<n;i++){
+    for (i = 0; i < n; i++) {
         printf("\tP%d\t%9d\t%7d\t%10d\t%9d\t%14d\n", i, at[i], bt[i], ct[i], wt[i], tat[i]);
     }
-    
+
     printf("\nGantt Chart:\n");
-    for(i=0;i<n;i++){
-        printf("P%d\t", i);
-    }
+    for (i = 0; i < n; i++) printf("P%d\t", i);
     printf("\n");
-    for(i=0;i<n;i++){
-        printf("%d\t", ct[i]);
-    }
+    for (i = 0; i < n; i++) printf("%d\t", ct[i]);
     printf("\n");
-    
+
     printf("\nAverage Waiting Time: %.2f\n", wtavg);
     printf("Average Turnaround Time: %.2f\n", tatavg);
-    
+
     return 0;
 }
